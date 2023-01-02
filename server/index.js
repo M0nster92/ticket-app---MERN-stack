1a0b2f2d0f3870b291061ee4ac292e2be04af847
M0nster92 <fahimalsafi@gmail.com>
2023-01-01 20:19:51 -0700

    deployment configuration

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 const connectDB = require("./config/db");
@@ -11,12 +12,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Ticket Api" });
-});
-
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use("*", (req, res) =>
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json({ message: "Ticket Api" });
+  });
+}
 
 app.use(errorHandler);
 
